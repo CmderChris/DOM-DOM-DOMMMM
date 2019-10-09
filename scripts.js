ad5225f1c5a2f4797042600025881ce1b06b6bbe
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-09 13:45:45 -0500

    on last part

--- scripts.js
+++ scripts.js
@@ -1,17 +1,44 @@
-let div = document.createElement("div");
-div.className = "header-container";
-document.body.appendChild(div);
+document.addEventListener('DOMContentLoaded', button);
+function button() {
+    let button = document.createElement('button');
+    let btnText =  document.createTextNode('Add a square!');
+    button.className = 'button';
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+    let id = 0;
 
+document.getElementsByClassName('button')[0].addEventListener('click', function() {
+    let square = document.createElement('div');
+    document.body.appendChild(square);
+    square.id = id++;
+    square.className = 'boxColor';
+    let p = document.createElement('p');
+    let squareText = document.createTextNode(id);
+    p.appendChild(squareText);
+    square.appendChild(p);
+    document.body.appendChild(square);
+    p.style.opacity = '0';
 
-let btn = document.createElement("button"); 
-let btnTxt = document.createTextNode("Add Square"); 
-btn.appendChild(btnTxt);
-btn.className = "btn btn-lg btn-primary";
-div.appendChild(btn);
+     square.addEventListener('mouseenter', hover);
+     function hover() {
+         p.style.opacity='1';
+     }
 
-document.getElementsByClassName('btn')[0].addEventListener('click', function() {
+     square.addEventListener('mouseleave', noHover);
+     function noHover() {
+         p.style.opacity='0';
+     }
 
-    let boxDiv = document.createElement('div');
-    document.body.appendChild(boxDiv);
-    boxDiv.id = 'box';
+     square.addEventListener('click', function() {
+        let myRandomColor = getRandomColor();
+        square.style.backgroundColor = myRandomColor;
+    });
 });
+
+function getRandomColor() {
+    let myColors = ['#F766C6', '#F7667D', '#F79766', '#F7E066', '#C6F766', '#7DF766'];
+    let myRandomColor = myColors[Math.floor(Math.random() * myColors.length)];
+    return myRandomColor; 
+};
+
+}
